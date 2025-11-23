d695a1e110e483c305df23790990c8d72f354bdc
csaezdecea <naitsirczeas@gmail.com>
2025-11-23 12:43:26 -0300

    palette

---
+++ build/CMakeFiles/fractal_app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fractal_app.dir/src/fractal_utils.cpp.o"
+  "CMakeFiles/fractal_app.dir/src/fractal_utils.cpp.o.d"
+  "CMakeFiles/fractal_app.dir/src/main.cpp.o"
+  "CMakeFiles/fractal_app.dir/src/main.cpp.o.d"
+  "fractal_app"
+  "fractal_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fractal_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/shaders/shader.vert
@@ -0,0 +1,9 @@
+#version 430 core
+layout(location = 0) in vec2 pos;
+layout(location = 1) in vec2 uv_in;
+out vec2 uv;
+void main()
+{
+    uv = uv_in;
+    gl_Position = vec4(pos, 0.0, 1.0);
+}
